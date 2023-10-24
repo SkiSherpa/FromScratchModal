f60ec86bcbe6f6a4e3b32f00af01ea238fc75e5b
Erick Watanabe <erickjwatanabe@gmail.com>
2023-10-24 14:25:55 -0700

    added title and body text to be dynamic and determined in app, instead of modal component

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import { useState } from 'react';
 import './App.css';
 import Modal2 from './modal2/modal2.jsx';
 
-function App() {
+function App({}) {
 
   const [ isOpen, setIsOpen ] = useState(false);
+
+  const bodyText = "This is the test body, I don't think that useState is needed as we don't update this text for this excersice"
 
   function toggleModal() {
     console.log("isOPen is now:", isOpen);
@@ -16,7 +18,8 @@ function App() {
     <>
       <h1>Modal Practice</h1>
       <button className="btn-modal" onClick={() => toggleModal()}>Open Modal</button>
-      <Modal2 isOpen={isOpen} onClose={toggleModal}/>
+      {/* Can pass in the title or any content as props */}
+      <Modal2 title="Modal 2, using notes" body={bodyText} isOpen={isOpen} onClose={toggleModal}/>
     </>
   );
 }
--- src/modal2/modal2.jsx
+++ src/modal2/modal2.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './modal2.css';
 
-export default function Modal2 ({isOpen, onClose}) {
+// Use the "title", so the title is dynamic,
+// but is determined by the parent component
+export default function Modal2 ({isOpen, onClose, title, body}) {
     return (
         isOpen && (
                 <div className="overlay">
                     <div className="modal">
-                        <h2>Modal title</h2>
-                        <p>body, body, body</p>
+                        <h2>{title}</h2>
+                        <p>{body}</p>
                         <button onClick={onClose}>
                             CLOSE MODAL
                         </button>
